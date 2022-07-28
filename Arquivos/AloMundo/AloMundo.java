19941f69616577c4b02f2c9880c2163a77543e54
lhrossi <lh_rossi@hotmail.com>
2022-07-28 19:59:10 -0300

    ,20220728

--- Arquivos/AloMundo/AloMundo.java
+++ Arquivos/AloMundo/AloMundo.java
@@ -1,5 +1,9 @@
 public class AloMundo{
     public static void main(String[] args) {
-        System.out.print("Alo Mundo!");
+        System.out.print("Alo Mundo! \n20220728 \nluisRossi\n");
+        System.out.println("20220728");
+        System.out.print("LuisRossi");
     }
+       
 }
+
