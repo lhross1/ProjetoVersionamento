f5a60f2f090dde00c61d94410e9bb7bdae481c65
lhrossi <lh_rossi@hotmail.com>
2022-07-28 21:47:14 -0300

    ,20220728

--- Arquivos/CalcularVelocidadeMedia/src/main/java/com/mycompany/calcularvelocidademedia/CalcularVelocidadeMedia.java
+++ Arquivos/CalcularVelocidadeMedia/src/main/java/com/mycompany/calcularvelocidademedia/CalcularVelocidadeMedia.java
@@ -21,7 +21,9 @@ public class CalcularVelocidadeMedia {
         System.out.println("Para encontrar a velocidade média " + "dividimos a distância (" + distanciaPercorrida + ") pelo tempo gasto no " + "percurso (" + tempoGasto + ").");
         System.out.println("A velocidade média é de " + velocidadeMedia + "Km/h");
         double consumoLitro = 10;
+        System.out.println("O carro faz " + consumoLitro + " km/L.");
         double custoLitro = 7;
+        System.out.println("O custo do litro do combustível é R$" + custoLitro + " reais.");
         double custoTotal = (distanciaPercorrida/consumoLitro)*custoLitro;
         System.out.println("O custo total da viagem é de R$" +custoTotal+ " reais");
     }
