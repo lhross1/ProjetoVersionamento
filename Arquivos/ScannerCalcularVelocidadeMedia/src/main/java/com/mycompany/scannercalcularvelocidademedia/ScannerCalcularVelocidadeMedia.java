fcfc9e405512b601b005dd97e003d9ab757756f0
LeonardoK <leonardo-kuhl@hotmail.com>
2022-08-01 22:02:44 -0300

    ,20220801

--- Arquivos/ScannerCalcularVelocidadeMedia/src/main/java/com/mycompany/scannercalcularvelocidademedia/ScannerCalcularVelocidadeMedia.java
+++ Arquivos/ScannerCalcularVelocidadeMedia/src/main/java/com/mycompany/scannercalcularvelocidademedia/ScannerCalcularVelocidadeMedia.java
@@ -21,23 +21,23 @@ public class ScannerCalcularVelocidadeMedia {
         double distanciaPercorrida = scanner.nextDouble();
         System.out.println("A distância percorrida foi de " + distanciaPercorrida + "Km.");
         
-        System.out.println("Digite o tempo gasto: ");
+        System.out.println("\nDigite o tempo gasto: ");
         double tempoGasto = scanner.nextDouble();
         System.out.println("O tempo gasto foi de " + tempoGasto + " horas.");
         
         double velocidadeMedia = distanciaPercorrida / tempoGasto;
         System.out.println("A velocidade média é de " + velocidadeMedia + "Km/h");
         
-        System.out.println("Digite o consumo do carro: ");
+        System.out.println("\nDigite o consumo do carro: ");
         double consumoCarro = scanner.nextDouble();
         System.out.println("O carro faz " + consumoCarro + " km/L.");
         
-        System.out.println("Digite o custo do litro de gasolina: ");
+        System.out.println("\nDigite o custo do litro de gasolina: ");
         double custoLitro = scanner.nextDouble();
-        System.out.println("O custo do litro do combustível é de R$" + custoLitro + " reais.");
+        System.out.println("\nO custo do litro do combustível é de R$" + custoLitro + " reais.");
         
         double custoTotal = (distanciaPercorrida / consumoCarro) * custoLitro;
-        System.out.println("O custo total da viagem é de R$" +custoTotal+ " reais.");
+        System.out.println("\nO custo total da viagem é de R$" +custoTotal+ " reais.");
         scanner.close();
         System.out.println("Fim!");
         System.out.println("=====================================================");
