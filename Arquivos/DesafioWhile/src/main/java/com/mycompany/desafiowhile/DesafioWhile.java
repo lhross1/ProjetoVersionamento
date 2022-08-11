0feed419dc46b68a8f358576691ce3cea8931780
lhrossi <lh_rossi@hotmail.com>
2022-08-10 22:16:41 -0300

    ,20220810

---
+++ Arquivos/DesafioWhile/src/main/java/com/mycompany/desafiowhile/DesafioWhile.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.desafiowhile;
+
+/**
+ *
+ * @author Aluno
+ */
+public class DesafioWhile {
+
+    public static void main(String[] args) {
+        
+       int numero = 100
+      
+        
+   if(numero % 2 = 0){
+   System.out.println("par");
+   } else {
+    System.out.println("[impar");
+   }
+   ;
+       
+       
+       /*
+       while(i<=100){
+       if(numero % 2 = 0){
+       System.out.println(numero);
+       }
+       numero = numero + i;
+       i++;
+       }
+      */
+    }
+}
